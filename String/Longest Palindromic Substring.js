871ad73e50e68bc49a8693a5bf169638049a7f13
Reegan Kumaran <reegank20@gmail.com>
2026-01-17 22:30:28 +0530

    feat: add Longest Palindromic Substring problem

---
+++ String/Longest Palindromic Substring.js
@@ -0,0 +1,12 @@
+/**
+ * Problem: Longest Palindromic Substring
+ * Given a string `s`, return the longest palindromic substring in `s`.
+ * 
+ * Output: String (the longest palindromic substring)
+ * 
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    
+};
