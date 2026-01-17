52141d2e8836a14c23cb3841afcb4e261f3599b3
Reegan Kumaran <reegank20@gmail.com>
2026-01-17 22:30:13 +0530

    feat: add Longest Substring Without Repeating Characters problem

---
+++ String/Longest Substring Without Repeating Characters.js
@@ -0,0 +1,12 @@
+/**
+ * Problem: Longest Substring Without Repeating Characters
+ * Given a string `s`, find the length of the longest substring without repeating characters.
+ * 
+ * Output: Integer (length of longest substring)
+ * 
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    
+};
