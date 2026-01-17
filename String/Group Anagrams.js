3e4d43913d8083eee9eca1a6e6da01c16937b7d3
Reegan Kumaran <reegank20@gmail.com>
2026-01-17 22:30:32 +0530

    feat: add Group Anagrams problem

---
+++ String/Group Anagrams.js
@@ -0,0 +1,13 @@
+/**
+ * Problem: Group Anagrams
+ * Given an array of strings `strs`, group the anagrams together. You can return the answer in any order.
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ * 
+ * Output: Array of arrays of strings (grouped anagrams)
+ * 
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    
+};
