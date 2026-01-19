a90da1e8beee6ec73d67b0f7dd2b9d47bf11c1b7
Reegan Kumaran <reegank20@gmail.com>
2026-01-19 23:10:29 +0530

    feat: add Reverse Linked List problem

---
+++ LinkedList/Reverse Linked List.js
@@ -0,0 +1,18 @@
+/**
+ * Problem: Reverse Linked List
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ * 
+ * Output: ListNode (head of the reversed list)
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ * 
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    
+};
