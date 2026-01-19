70231851e0b646c6e2ce0cd08d6007603ced7a61
Reegan Kumaran <reegank20@gmail.com>
2026-01-19 23:10:51 +0530

    feat: add Linked List Cycle problem

---
+++ LinkedList/Linked List Cycle.js
@@ -0,0 +1,20 @@
+/**
+ * Problem: Linked List Cycle
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+ * Return `true` if there is a cycle in the linked list. Otherwise, return `false`.
+ * 
+ * Output: Boolean (`true` if cycle exists, `false` otherwise)
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ * 
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    
+};
