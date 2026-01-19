5cc7e8b54004fbb9062c8e702cca08af29eba95b
Reegan Kumaran <reegank20@gmail.com>
2026-01-19 23:11:02 +0530

    feat: add Remove Nth Node From End of List problem

---
+++ LinkedList/Remove Nth Node From End of List.js
@@ -0,0 +1,19 @@
+/**
+ * Problem: Remove Nth Node From End of List
+ * Given the head of a linked list, remove the `n`th node from the end of the list and return its head.
+ * 
+ * Output: ListNode (head of the list after removal)
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ * 
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    
+};
