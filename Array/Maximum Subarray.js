82db48954c7cddc67df2e84670ce51d8ac68365c
Reegan Kumaran <reegank20@gmail.com>
2026-01-27 17:25:37 +0530

    Maximum Subarray

--- Array/Maximum Subarray.js
+++ Array/Maximum Subarray.js
@@ -8,5 +8,13 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    
+    let currentSum = 0;
+    let maxSum = 0;
+    for(let i = 0; i < nums.length; i++){
+        currentSum = Math.max(nums[i], currentSum + nums[i]);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+    return maxSum;
 };
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1]));
