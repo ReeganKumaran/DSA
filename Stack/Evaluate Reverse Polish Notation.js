03b4ea2376719eb28cb6f0ea2aca19eaae1eb00e
Reegan Kumaran <reegank20@gmail.com>
2026-01-29 23:41:54 +0530

    feat: add Evaluate Reverse Polish Notation problem

---
+++ Stack/Evaluate Reverse Polish Notation.js
@@ -0,0 +1,18 @@
+/**
+ * Problem: Evaluate Reverse Polish Notation
+ * You are given an array of strings `tokens` that represents an arithmetic expression in a Reverse Polish Notation.
+ * Evaluate the expression. Return an integer that represents the value of the expression.
+ *
+ * Note that:
+ * - The valid operators are '+', '-', '*', and '/'.
+ * - Each operand may be an integer or another expression.
+ * - The division between two integers always truncates toward zero.
+ * - There will not be any division by zero.
+ * - The input represents a valid arithmetic expression in a reverse polish notation.
+ *
+ * Output: Integer (result of the expression)
+ *
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {};
