31ca05f81238eaf7791eef203b4e828e85daca1f
Reegan Kumaran <reegank20@gmail.com>
2026-01-29 23:42:02 +0530

    feat: add Generate Parentheses problem

---
+++ Stack/Generate Parentheses.js
@@ -0,0 +1,10 @@
+/**
+ * Problem: Generate Parentheses
+ * Given `n` pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
+ * Output: Array of strings (all valid combinations)
+ *
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {};
