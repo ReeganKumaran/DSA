29bf8ca83e8c155e4606b1136525342216d5696d
Reegan Kumaran <reegank20@gmail.com>
2026-01-29 23:41:46 +0530

    feat: add Min Stack problem

---
+++ Stack/Min Stack.js
@@ -0,0 +1,36 @@
+/**
+ * Problem: Min Stack
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ *
+ * Implement the `MinStack` class:
+ * - `MinStack()` initializes the stack object.
+ * - `void push(int val)` pushes the element `val` onto the stack.
+ * - `void pop()` removes the element on the top of the stack.
+ * - `int top()` gets the top element of the stack.
+ * - `int getMin()` retrieves the minimum element in the stack.
+ *
+ * Output: Void (operations on the stack)
+ */
+
+var MinStack = function () {};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {};
