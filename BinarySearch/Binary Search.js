208ea4265032f1925d84f3600dba2e0729ce12cc
Reegan Kumaran <reegank20@gmail.com>
2026-01-31 23:56:03 +0530

    feat: add Binary Search problem

---
+++ BinarySearch/Binary Search.js
@@ -0,0 +1,13 @@
+/**
+ * Problem: Binary Search
+ * Given an array of integers `nums` which is sorted in ascending order, and an integer `target`, write a function to search `target` in `nums`.
+ * If `target` exists, then return its index. Otherwise, return `-1`.
+ * You must write an algorithm with O(log n) runtime complexity.
+ *
+ * Output: Integer (index of target or -1)
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {};
