f03a7d53a86791645e464ff81becb963b9c60908
Reegan Kumaran <reegank20@gmail.com>
2026-01-31 23:56:36 +0530

    feat: add Search in Rotated Sorted Array problem

---
+++ BinarySearch/Search in Rotated Sorted Array.js
@@ -0,0 +1,14 @@
+/**
+ * Problem: Search in Rotated Sorted Array
+ * There is an integer array `nums` sorted in ascending order (with distinct values).
+ * Prior to being passed to your function, `nums` is possibly rotated at an unknown pivot index `k` (1 <= k < nums.length).
+ * Given the array `nums` after the possible rotation and an integer `target`, return the index of `target` if it is in `nums`, or `-1` if it is not in `nums`.
+ * You must write an algorithm with O(log n) runtime complexity.
+ *
+ * Output: Integer (index of target or -1)
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {};
