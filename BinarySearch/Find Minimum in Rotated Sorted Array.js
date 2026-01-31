61eec90f54947bbf5d6d30bac3dd2aeb9107f6ee
Reegan Kumaran <reegank20@gmail.com>
2026-01-31 23:56:28 +0530

    feat: add Find Minimum in Rotated Sorted Array problem

---
+++ BinarySearch/Find Minimum in Rotated Sorted Array.js
@@ -0,0 +1,12 @@
+/**
+ * Problem: Find Minimum in Rotated Sorted Array
+ * Suppose an array of length `n` sorted in ascending order is rotated between `1` and `n` times.
+ * Given the sorted rotated array `nums` of unique elements, return the minimum element of this array.
+ * You must write an algorithm that runs in O(log n) time.
+ *
+ * Output: Integer (minimum element)
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {};
