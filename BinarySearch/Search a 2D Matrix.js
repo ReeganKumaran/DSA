ff87fabe01d7b5721b04dd0825ed1cc978d747f4
Reegan Kumaran <reegank20@gmail.com>
2026-01-31 23:56:11 +0530

    feat: add Search a 2D Matrix problem

---
+++ BinarySearch/Search a 2D Matrix.js
@@ -0,0 +1,15 @@
+/**
+ * Problem: Search a 2D Matrix
+ * You are given an `m x n` integer matrix `matrix` with the following two properties:
+ * 1. Each row is sorted in non-decreasing order.
+ * 2. The first integer of each row is greater than the last integer of the previous row.
+ * Given an integer `target`, return `true` if `target` is in `matrix` or `false` otherwise.
+ * You must write a solution in O(log(m * n)) time complexity.
+ *
+ * Output: Boolean (`true` if found, `false` otherwise)
+ *
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {};
