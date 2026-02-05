c5708d9aaee4c33a5a2f2b0408a8d2f9a6439195
Reegan Kumaran <reegank20@gmail.com>
2026-02-05 23:22:28 +0530

    feat: add Permutation in String problem

---
+++ SlidingWindow/Permutation in String.js
@@ -0,0 +1,12 @@
+/**
+ * Problem: Permutation in String
+ * Given two strings `s1` and `s2`, return `true` if `s2` contains a permutation of `s1`, or `false` otherwise.
+ * In other words, return `true` if one of `s1`'s permutations is the substring of `s2`.
+ *
+ * Output: Boolean (`true` if found, `false` otherwise)
+ *
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function (s1, s2) {};
