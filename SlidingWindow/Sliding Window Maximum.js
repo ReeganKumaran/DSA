4510f37f3831e85fd1b88c4a292c35a8f589e66e
Reegan Kumaran <reegank20@gmail.com>
2026-02-05 23:22:43 +0530

    feat: add Sliding Window Maximum problem

---
+++ SlidingWindow/Sliding Window Maximum.js
@@ -0,0 +1,12 @@
+/**
+ * Problem: Sliding Window Maximum
+ * You are given an array of integers `nums`, there is a sliding window of size `k` which is moving from the very left of the array to the very right. You can only see the `k` numbers in the window. Each time the sliding window moves right by one position.
+ * Return the max sliding window.
+ *
+ * Output: Array of integers (max element in each window)
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {};
