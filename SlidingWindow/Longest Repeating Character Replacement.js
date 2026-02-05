8a6e9ff4c42216edb7ba92fb9cf368cd4f28bded
Reegan Kumaran <reegank20@gmail.com>
2026-02-05 23:22:19 +0530

    feat: add Longest Repeating Character Replacement problem

---
+++ SlidingWindow/Longest Repeating Character Replacement.js
@@ -0,0 +1,12 @@
+/**
+ * Problem: Longest Repeating Character Replacement
+ * You are given a string `s` and an integer `k`. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most `k` times.
+ * Return the length of the longest substring containing the same letter you can get after performing the above operations.
+ *
+ * Output: Integer (length of the longest substring)
+ *
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function (s, k) {};
